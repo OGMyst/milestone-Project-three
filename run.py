0f4ebbf4485aeccfb182e236e6bbc2a33cfc71b8
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-06-15 15:03:33 +0000

    Moved css file to static directory

--- run.py
+++ run.py
@@ -1,14 +1,28 @@
 import datetime
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
 
 
-date_select = str(datetime.date.today())
-split_date = date_select.split("-")
-current_month = int(split_date[1]) - 1
+@app.route('/')
+def home():
+    return render_template("index.html")
 
-month_names = ("January", "February", "March", "April",
-               "May", "June", "July", "August", "September",
-               "Octboer", "November", "Decemeber")
+    date_select = str(datetime.date.today())
+    split_date = date_select.split("-")
+    current_month = int(split_date[1]) - 1
 
-month_select = month_names[current_month]
+    month_names = ("January", "February", "March", "April",
+                   "May", "June", "July", "August", "September",
+                   "Octboer", "November", "Decemeber")
 
-print(month_select)
+    month_select = month_names[current_month]
+
+    print(month_select)
+
+
+url_for("index.html")
+
+
+if __name__ == "__main__":
+    app.run()

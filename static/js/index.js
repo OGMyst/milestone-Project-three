7a474b443ba9f3ac582a48713559ff55c27596f9
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-06-15 16:33:14 +0000

    Added functions to display current month and days of the week for calendar

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() { 
-    let today = new Date()
-    let month = today.getMonth()
-    let monthNames = ["January", "February", "March", "April",
-                   "May", "June", "July", "August", "September",
-                   "Octboer", "November", "Decemeber"]
-    console.log(monthNames[month])
-    currentMonth = monthNames[month]
-    document.getElementById("month").innerHTML = currentMonth
+    calendarMonth();
+    daysOfTheWeek();
 });
+
+function calendarMonth(){
+    let today = new Date();
+    let month = today.getMonth();
+    let monthNames = ["January", "February", "March", "April",
+                    "May", "June", "July", "August", "September",
+                    "Octboer", "November", "Decemeber"];
+    currentMonth = monthNames[month];
+    document.getElementById("month").innerHTML = "<h1>" + currentMonth + "</h1>";
+};
+
+function daysOfTheWeek(){
+     let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    let weekDays;
+    for(i = 0; i < 7; i++){
+        weekDays = days[i];
+        document.getElementById("calendar").innerHTML += "<div class='week-days'>" + weekDays + "</div>"
+    }
+};

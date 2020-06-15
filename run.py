459a3635ab5c25ddb47af3cf039090f0d499f99a
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-06-15 16:11:06 +0000

    Calenedar uses index.js to print current month

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-import datetime
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
@@ -6,16 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    date_select = str(datetime.date.today())
-    split_date = date_select.split("-")
-    current_month = int(split_date[1]) - 1
-
-    month_names = ("January", "February", "March", "April",
-                   "May", "June", "July", "August", "September",
-                   "Octboer", "November", "Decemeber")
-
-    month_select = month_names[current_month]
-    print(month_select)
     return render_template("index.html")
 
 
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  let month = today.getMonth()
-  document.getElementById("month").innerHTML = month
-  console.log(month)
+document.addEventListener("DOMContentLoaded", function() { 
+    let today = new Date()
+    let month = today.getMonth()
+    let monthNames = ["January", "February", "March", "April",
+                   "May", "June", "July", "August", "September",
+                   "Octboer", "November", "Decemeber"]
+    console.log(monthNames[month])
+    currentMonth = monthNames[month]
+    document.getElementById("month").innerHTML = currentMonth
 });

a1f0a769c186fe54f09e06a7df58d403d39adfcd
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-06-16 13:09:00 +0000

    Setup jasmine testing

--- static/js/index.js
+++ static/js/index.js
@@ -6,11 +6,14 @@ document.addEventListener("DOMContentLoaded", function() {
 function calendarMonth(){
     let today = new Date();
     let month = today.getMonth();
+    let year = today.getFullYear();
+    showCalendar(month, year);
     let monthNames = ["January", "February", "March", "April",
                     "May", "June", "July", "August", "September",
                     "Octboer", "November", "Decemeber"];
     currentMonth = monthNames[month];
     document.getElementById("month").innerHTML = "<h1>" + currentMonth + "</h1>";
+    
 };
 
 function daysOfTheWeek(){
@@ -21,3 +24,10 @@ function daysOfTheWeek(){
         document.getElementById("day-labels").innerHTML += "<div class='week-days'>" + weekDays + "</div>"
     }
 };
+
+function showCalendar(month, year) {
+
+    let firstDay = (new Date(year, month)).getDay();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+    console.log(daysInMonth)
+}

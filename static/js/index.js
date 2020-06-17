f3b3a10709caf2504545b245d72d45db71461628
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-06-17 13:27:27 +0000

    Added showCalender() with jasmine testing

--- static/js/index.js
+++ static/js/index.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() { 
     calendarMonth();
     daysOfTheWeek();
+    showCalendar(month, year)
 });
 
-function calendarMonth(){
-    let today = new Date();
-    let month = today.getMonth();
-    let year = today.getFullYear();
-    showCalendar(month, year);
-    let monthNames = ["January", "February", "March", "April",
-                    "May", "June", "July", "August", "September",
-                    "Octboer", "November", "Decemeber"];
+let today = new Date();
+let month = today.getMonth();
+let year = today.getFullYear();
+let monthNames = ["January", "February", "March", "April",
+                  "May", "June", "July", "August", "September",
+                  "Octboer", "November", "Decemeber"];
+let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+let weekDays;
+
+function calendarMonth(){ 
     currentMonth = monthNames[month];
-    document.getElementById("month").innerHTML = "<h1>" + currentMonth + "</h1>";
-    
+    document.getElementById("month").innerHTML = "<h1>" + currentMonth + "</h1>";    
 };
 
-function daysOfTheWeek(){
-     let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-    let weekDays;
+function daysOfTheWeek(){    
     for(i = 0; i < 7; i++){
         weekDays = days[i];
         document.getElementById("day-labels").innerHTML += "<div class='week-days'>" + weekDays + "</div>"
@@ -29,5 +29,34 @@ function showCalendar(month, year) {
 
     let firstDay = (new Date(year, month)).getDay();
     let daysInMonth = 32 - new Date(year, month, 32).getDate();
-    console.log(daysInMonth)
+    let tbl = document.getElementById("calendar-days");
+    // clearing all previous cells
+    tbl.innerHTML = "";
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+        let row = document.createElement("tr");
+
+        for (let j = 1; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode("");
+                cell.classList.add("calendar-cell")
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysInMonth) {
+                break;
+            }
+            else {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode(date);
+                cell.classList.add("calendar-cell")
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+        }
+        tbl.appendChild(row); // appending each row into calendar body.
+    }
 }

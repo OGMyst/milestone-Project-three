f4ac3913a8d53c7cf45884b6f4ebad30c9dc0f50
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-06-25 13:00:18 +0000

    Fixed bug where the poster for first card clicked would always be poster displayed on modal

--- static/js/films.js
+++ static/js/films.js
@@ -1,17 +1,25 @@
+let expandedCardImage ;
+let currentId ;
 
 function viewMoreModal(){   
     $("#modal-background").css("display", "flex");
+
     let identifyInstance = event.target
     let findCurrentParent = $(identifyInstance).parents(".film-card")
     findCurrentParent[0].id = "current-card";
+    currentId = findCurrentParent[0].id;
+    
     let parentId = document.getElementById("current-card");
     let posterelement = $(parentId).find("img");
     let expandedCardImage = document.getElementById("expanded-card-image")
+    
     expandedCardImage.src = posterelement[0].src
-    console.log(posterelement[0].src)
+    let modalPoster = expandedCardImage.src
+    return modalPoster
 };
 
 function closeModal(){
     $("#modal-background").css("display", "none");
+    document.getElementById("current-card").id = "not-current";
 }
 

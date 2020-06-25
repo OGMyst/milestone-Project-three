06482cbe0c3b486efab33ba94176e28efe88ac3b
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-06-25 13:13:55 +0000

    Refactored viewMoreModal()

--- static/js/films.js
+++ static/js/films.js
@@ -1,21 +1,7 @@
-let expandedCardImage ;
-let currentId ;
-
 function viewMoreModal(){   
     $("#modal-background").css("display", "flex");
-
-    let identifyInstance = event.target
-    let findCurrentParent = $(identifyInstance).parents(".film-card")
-    findCurrentParent[0].id = "current-card";
-    currentId = findCurrentParent[0].id;
-    
-    let parentId = document.getElementById("current-card");
-    let posterelement = $(parentId).find("img");
-    let expandedCardImage = document.getElementById("expanded-card-image")
-    
-    expandedCardImage.src = posterelement[0].src
-    let modalPoster = expandedCardImage.src
-    return modalPoster
+    giveCurrentCardId();
+    displayFilmInfo();
 };
 
 function closeModal(){
@@ -23,3 +9,15 @@ function closeModal(){
     document.getElementById("current-card").id = "not-current";
 }
 
+function giveCurrentCardId(){
+    let identifyInstance = event.target
+    let findCurrentParent = $(identifyInstance).parents(".film-card")
+    findCurrentParent[0].id = "current-card";
+}
+
+function displayFilmInfo(){
+    let parentId = document.getElementById("current-card");
+    let posterelement = $(parentId).find("img");
+    let expandedCardImage = document.getElementById("expanded-card-image")
+    expandedCardImage.src = posterelement[0].src
+}

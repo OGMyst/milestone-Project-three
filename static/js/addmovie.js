39556dbe4a45d29107a29626544b58334aae8eac
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-06-26 14:40:09 +0000

    Add more button now toggles additional-info

--- static/js/addmovie.js
+++ static/js/addmovie.js
@@ -1,3 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() { 
+    $("#additional-info").hide();
+    $("#notification-box").hide();
+});
+
+function showMoreInputs(){
+    $("#additional-info").toggle("slow");
+    $("#notification-box").toggle("slow");
+};
+
 function submitForm() {
   document.forms[0].submit();
 };

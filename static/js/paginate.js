181e9348274a9baabdaabf0706320a97101e7f76
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-07-11 15:32:06 +0000

    Added pagination functionality to films.html

--- static/js/paginate.js
+++ static/js/paginate.js
@@ -1,18 +1,48 @@
 let filmCards = document.getElementsByClassName("film-card");
-let filmContainer = document.getElementById("film-container");
+// let filmContainer = document.getElementById("film-container");
 let current_page = 1;
 let filmPerPage = 5;
 document.addEventListener("DOMContentLoaded", function() { 
-    displayFilms (filmCards, filmContainer, filmPerPage, current_page);
+    displayFilms (filmCards, filmPerPage, current_page);
     
 });
 
-function displayFilms (films, container, films_per_page, pageNumber) {
+function displayFilms (films, films_per_page, pageNumber) {
 	pageNumber--;
 	let start = films_per_page * pageNumber;
     let end = start + films_per_page;
     
 	for (let i = start; i < end; i++) {
-		$(films[i]).removeClass("hidden")
+        $(films[i]).removeClass("hidden");
+        $(films[i]).addClass("current-films");
+        
 	}
 };
+
+function nextPage(){
+    let currentFilms = document.getElementsByClassName("current-films");
+    hideCurrentFilms(currentFilms);
+    removeCurrentFilmsClass(currentFilms);
+    current_page++;
+    displayFilms (filmCards, filmPerPage, current_page);    
+};
+
+function previousPage(){
+    let currentFilms = document.getElementsByClassName("current-films");
+    hideCurrentFilms(currentFilms);
+    removeCurrentFilmsClass(currentFilms);
+    current_page--;
+    displayFilms (filmCards, filmPerPage, current_page);    
+};
+
+function hideCurrentFilms(currentFilms){
+    for(i = 0; i < currentFilms.length; i++){  
+        $(currentFilms[i]).addClass("hidden");       
+    };
+}
+
+function removeCurrentFilmsClass(currentFilms){
+    for(i = 0; i < currentFilms.length; i++){  
+        $(currentFilms[i]).removeClass("current-films");       
+    };
+}

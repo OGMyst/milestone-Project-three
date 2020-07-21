27a216d2b7aac68046ebac849c1350b198f7b7e6
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-07-21 14:17:29 +0000

    Changed variable names to be more descriptive. formated dateURL to provide correct link

--- static/js/index.js
+++ static/js/index.js
@@ -83,27 +83,27 @@ function showCalendar(month, year, films) {
 }
 
 function createFilledCell(date, row, row_count, i, endofRow){
-    let cell = document.createElement("td");
-    let div = document.createElement("div");
+    let calendarCell = document.createElement("td");
+    let calendarCellDiv = document.createElement("div");
     let dateTag = document.createElement("a");
-    let pTag = document.createElement("p");
+    let cellTextTag = document.createElement("p");
     let cellText = date;
     if(i === (row_count - 1)){
-        cell.classList.add("bottom-row-cell");
+        calendarCell.classList.add("bottom-row-cell");
     }
 
     if(endofRow === 0){
-        cell.classList.add("end-row-cell");
+        calendarCell.classList.add("end-row-cell");
     }
 
-    cell.classList.add("calendar-cell");               
-    div.classList.add("calendar-cell-div");
-    cell.append(div);
-    div.append(dateTag);
-    dateTag.append(pTag);
-    pTag.append(cellText);
-    pTag.classList.add("date-text");
-    row.appendChild(cell);
+    calendarCell.classList.add("calendar-cell");               
+    calendarCellDiv.classList.add("calendar-cell-div");
+    calendarCell.append(calendarCellDiv);
+    calendarCellDiv.append(dateTag);
+    dateTag.append(cellTextTag);
+    cellTextTag.append(cellText);
+    cellTextTag.classList.add("date-text");
+    row.appendChild(calendarCell);
 }
 
 function nextmonth() {
@@ -160,7 +160,8 @@ function insertImagesToCalendar(films, daysInMonth, month){
             let eachOccupiedCell = document.getElementsByClassName("calendar-cell-div");
             let formatedMonth = MONTHS_IN_DATE_FORMAT[month];    
             let allDaysInMonth = [];
-            let dateUrl = (`/date_of_film/${films[x].release_date}`);
+            let formatDate = films[x].release_date.replaceAll("/", "_")
+            let dateUrl = (`/date_of_film/${formatDate}`);
 
             for(i = 1; i <= daysInMonth; i++){
                 eachDate = DAYS_IN_DATE_FORMAT[i] + "/" + formatedMonth + "/" + year; 
@@ -168,24 +169,24 @@ function insertImagesToCalendar(films, daysInMonth, month){
             }
             for(j = 1; j <= allDaysInMonth.length; j++){
                 if(films[x].release_date === allDaysInMonth[j]){ 
-                    let image = document.createElement("img"); 
-                    let div = eachOccupiedCell[j]; 
-                    let imageLimit =  $(div).children();
+                    let calendarCellImage = document.createElement("img"); 
+                    let calendarCellDiv = eachOccupiedCell[j]; 
+                    let imageLimit =  $(calendarCellDiv).children();
                     let datePage = $(eachOccupiedCell[j]).find("a");
                         
                         
-                    image.setAttribute("onclick",`viewMoreModal('/edit_film/${films[x]._id}', '/delete_film/${films[x]._id}', '${films[x].theatrical_poster_url}', '${films[x].producer}', '${films[x].director}', '${films[x].duration}', '${films[x].film_name}', '${films[x].genre}', '${films[x].release_date}', '${films[x].screenplay}', '${films[x].story}', '${films[x].starring}', "${films[x].plot_summary}")`);
-                    image.setAttribute("alt", `${films[x].film_name}`);
-                    image.classList.add("calendar-image", "grow");
-                    image.src = films[x].theatrical_poster_url;
+                    calendarCellImage.setAttribute("onclick",`viewMoreModal('/edit_film/${films[x]._id}', '/delete_film/${films[x]._id}', '${films[x].theatrical_poster_url}', '${films[x].producer}', '${films[x].director}', '${films[x].duration}', '${films[x].film_name}', '${films[x].genre}', '${films[x].release_date}', '${films[x].screenplay}', '${films[x].story}', '${films[x].starring}', "${films[x].plot_summary}")`);
+                    calendarCellImage.setAttribute("alt", `${films[x].film_name}`);
+                    calendarCellImage.classList.add("calendar-image", "grow");
+                    calendarCellImage.src = films[x].theatrical_poster_url;
                     datePage[0].href = dateUrl;
                         
 
                     if(imageLimit.length < 3){
-                        div.append(image);
+                        calendarCellDiv.append(calendarCellImage);
                     }else{
-                        image.classList.add("hidden");
-                        div.append(image); 
+                        calendarCellImage.classList.add("hidden");
+                        calendarCellDiv.append(calendarCellImage); 
                     }            
                 } 
             }

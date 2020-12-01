db4230a26fe071b4893755d2f0a91cc828111a2a
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-12-01 16:55:41 +0000

    Added code to try and successful call GA

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,6 @@ $(document).ready(function(){
         $('select').formSelect();
         $('.sidenav').sidenav();
 });
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}

5d2bc18c5ef668651ee0ccaa8fd8e99d2e1cd6df
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-12-01 16:33:35 +0000

    Added js to test creating event for GA

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,7 @@ $(document).ready(function(){
         $('select').formSelect();
         $('.sidenav').sidenav();
 });
+
+function filmsTag(location){
+    datalayer.push({event: 'film_page', userId: location})
+}
